9343058d89ef4515a3341d53a703129fa0f1a7b5
alisootoodeh <alisotudehsjn@yahoo.com>
2020-12-28 16:14:20 +0330

    add error handling logic

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,11 +1,11 @@
 package users
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/AliSootoodeh/go-micro-api/domain/users"
 	"github.com/AliSootoodeh/go-micro-api/services"
+	"github.com/AliSootoodeh/go-micro-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,13 +13,13 @@ import (
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		//TODO
+		restErr := errors.NewBadRequestError("Invalid Json Body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		fmt.Println(saveErr)
-		//TODO
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
 	c.JSON(http.StatusCreated, result)
--- services/users_service.go
+++ services/users_service.go
@@ -2,9 +2,10 @@ package services
 
 import (
 	"github.com/AliSootoodeh/go-micro-api/domain/users"
+	"github.com/AliSootoodeh/go-micro-api/utils/errors"
 )
 
 // CreateUser ...
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
 }
