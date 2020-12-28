917387d7491d27350eac67033d7f620847831ac0
alisootoodeh <alisotudehsjn@yahoo.com>
2020-12-28 20:40:52 +0330

    dao and dto structure

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/AliSootoodeh/go-micro-api/domain/users"
 	"github.com/AliSootoodeh/go-micro-api/services"
@@ -25,9 +26,20 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, result)
 }
 
-// GetUser TODO
+// GetUser ...
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "TODO!!")
+	userID, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("userId should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userID)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 // SearchUser TODO
--- services/users_service.go
+++ services/users_service.go
@@ -5,7 +5,22 @@ import (
 	"github.com/AliSootoodeh/go-micro-api/utils/errors"
 )
 
+// GetUser ...
+func GetUser(userID int64) (*users.User, *errors.RestErr) {
+	result := &users.User{ID: userID}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 // CreateUser ...
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
 	return &user, nil
 }
