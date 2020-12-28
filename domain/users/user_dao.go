3f57ac551cfedacd8c7b408c540380121facf5f0
alisootoodeh <alisotudehsjn@yahoo.com>
2020-12-28 20:41:31 +0330

    dao and dto structure

---
+++ domain/users/user_dao.go
@@ -0,0 +1,16 @@
+package users
+
+import (
+	"github.com/AliSootoodeh/go-micro-api/utils/errors"
+)
+
+// Get ...
+func (user *User) Get() *errors.RestErr {
+
+	return nil
+}
+
+//Save ...
+func (user *User) Save() *errors.RestErr {
+	return nil
+}
