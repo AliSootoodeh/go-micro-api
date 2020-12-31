34fe65fcff34371024a35b9c6cb885cc7462408c
alisootoodeh <alisotudehsjn@yahoo.com>
2020-12-31 13:10:18 +0330

    add mySql logic

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,15 +4,30 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	mysqlUsersUsername = "mysql_users_username"
+	mysqlUsersPassword = "mysql_users_password"
+	mysqlUsersHost     = "mysql_users_host"
+	mysqlUsersSchema   = "mysql_users_schema"
 )
 
 var (
 	//UsersDB ...
 	UsersDB *sql.DB
+
+	username = os.Getenv("mysqlUsersUsername")
+	password = os.Getenv("mysqlUsersPassword")
+	host     = os.Getenv("mysqlUsersHost")
+	schema   = os.Getenv("mysqlUsersSchema")
 )
 
 func init() {
-	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "root", "127.0.0.1", "users_db")
+	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
 	var err error
 	UsersDB, err = sql.Open("mysql", datasourceName)
 	if err != nil {
